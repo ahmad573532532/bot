07aeaf87999abc031dc6708a610042977070db8f
ahmad573532532 <111409168+ahmad573532532@users.noreply.github.com>
2022-08-17 09:30:10 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
-global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
-global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
+global.Owner = ['923497985307']
+global.OwnerNumber = ['923497985307']
+global.ownertag = ['923497985307']
+global.BotName = "Ahmad Bot"
+global.packname = "Ahmad bot"
+global.author = "By: Ahmad"
+global.OwnerName = "Ahmad"
+global.BotSourceCode = ""
+global.SupportGroupLink = ""
 global.sessionName = "session"
 
 // Prefix //
@@ -23,7 +23,7 @@ global.sessionName = "session"
 global.prefa = ['-'] 
 
 
-global.location = "West Bengal, India"
+global.location = "Punjab, Pakistan"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
